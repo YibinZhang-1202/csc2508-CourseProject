b3675e6c582421b30ebb4b38be5d97c8968d2058
YibinZhang-1202 <576936894@qq.com>
2019-11-25 17:11:45 -0500

    Finish result process.

---
+++ Result_process/run.sh
@@ -0,0 +1,5 @@
+python3 result_process.py \
+	-dir-path ../aic19-track2-reid/t/ \
+	-detect-result detect_result.txt \
+	-reid-result-self reid_result_self.txt \
+	-reid-result-cross reid_result_cross.txt
