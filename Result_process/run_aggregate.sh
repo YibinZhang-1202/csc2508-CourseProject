5c7eae6181d3f1b1f0c5a341c90f9cf5e66d07f0
YibinZhang-1202 <576936894@qq.com>
2019-12-07 22:17:45 -0500

    Add extracting tracklet

---
+++ Result_process/run_aggregate.sh
@@ -0,0 +1,7 @@
+python3 aggregate.py \
+	-dir-path ../aic19-track2-reid/small_example/ \
+	-detect-result detect_result.txt \
+	-reid-result-self reid_result_self.txt \
+	-reid-result-cross reid_result_cross.txt \
+	-interval-size 5 \
+	-batch-size 100
